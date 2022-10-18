1325e4362bb8ecd38049d149bafc9e904d3b30be
LeighannaSB <107161421+LeighannaSB@users.noreply.github.com>
2022-10-17 18:36:58 -0700

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+head(MechaCar_table)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))

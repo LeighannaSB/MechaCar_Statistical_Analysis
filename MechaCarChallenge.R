e0e28dab2ba8fcaab1ca4e5112faee8c416fcdce
LeighannaSB <107161421+LeighannaSB@users.noreply.github.com>
2022-10-17 19:56:27 -0700

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,11 @@ MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactor
 head(MechaCar_table)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
+
+
+Sus_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+head(Sus_table)
+total_summary <- Sus_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+total_summary
+lot_summary <- Sus_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_summary
